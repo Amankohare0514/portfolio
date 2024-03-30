078a27a75e589c9a537886ae75801cf69f2bd62a
Aman kohare <amankohare@gmail.com>
2024-03-30 19:58:07 +0530

    education added and many more things add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { NextUIProvider } from '@nextui-org/react';
 import Hero from './home/page';
 import About from './about/page';
 import Education from './education/page';
-import Contact from './contact/page';
+
 export default function Home() {
   return (
     <>
