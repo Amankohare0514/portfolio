a174733bb1ba55b46b78d871a33e3fc031f5a001
Aman kohare <amankohare@gmail.com>
2024-09-22 19:45:42 +0530

    chore: blogs section

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["miro.medium.com", "i.ytimg.com", "i.ibb.co" , "heyaman.vercel.app"],
+        domains: ["miro.medium.com", "i.ytimg.com", "i.ibb.co" , "heyaman.vercel.app" ,"www.nextjs.cn" , "www.wallpapertip.com"],
     },
 };
 
