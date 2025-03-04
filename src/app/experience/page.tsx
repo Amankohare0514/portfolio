5379ddf6032696617876922e7e354eb6f06f5eb7
Aman kohare <amankohare@gmail.com>
2025-03-04 11:31:53 +0530

    change experience

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -5,6 +5,28 @@ import { Timeline } from "@/components/ui/timeline";
 function Experience() {
     const data = [
         {
+            title: "[March 2024 - Present]",
+            content: (
+                <div>
+                    <p className="text-neutral-800 dark:text-neutral-200 text-xl font-bold md:text-2xl mb-2">
+                        Techloom Solutions Private Limited
+                    </p>
+                    <p className="text-neutral-800 dark:text-neutral-200 text-xl font-semibold md:text-2xl mb-2">
+                        Co-founder & CEO - Bhopal
+                    </p>
+                    <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-2">
+                    At Techloom Solutions, we're not just another tech company ? we're your partners in progress. With a relentless commitment to excellence and a passion for innovation, we specialize in crafting bespoke solutions that exceed expectations.
+                    </p>
+                    <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-2">
+                        We develop robust Mobile and web solutions that transform businesses into online management tools. We are a group of highly motivated IT professionals with ability to innovate and a strong desire to excel.
+                    </p>
+                    <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-2">
+                        From start ups to established businesses, we help people transform their mobile and web presence into an asset by creating competitive advantages through e-strategy, development, rapid deployment, and continuous management.
+                    </p>
+                </div>
+            ),
+        },
+        {
             title: "[Sep 2024 - Jan 2025]",
             content: (
                 <div>
@@ -25,28 +47,6 @@ function Experience() {
                     </p>
                     <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-2">
                         Developed and maintained interactive user interfaces using React.js, enhancing user engagement and satisfaction.
-                    </p>
-                </div>
-            ),
-        },
-        {
-            title: "[Aug 2024 - Sep 2024]",
-            content: (
-                <div>
-                    <p className="text-neutral-800 dark:text-neutral-200 text-xl font-bold md:text-2xl mb-2">
-                        Dhakad Technosoft Private Limited
-                    </p>
-                    <p className="text-neutral-800 dark:text-neutral-200 text-xl font-semibold md:text-2xl mb-2">
-                        React Native developer - Indore, india
-                    </p>
-                    <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-2">
-                        Dhakad Technosoft is a rapidly growing, new generation IT company with extensive experience in designing and developing cutting edge custom software solutions and e-Business solutions.
-                    </p>
-                    <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-2">
-                        We develop robust Mobile and web solutions that transform businesses into online management tools. We are a group of highly motivated IT professionals with ability to innovate and a strong desire to excel.
-                    </p>
-                    <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-2">
-                        From start ups to established businesses, we help people transform their mobile and web presence into an asset by creating competitive advantages through e-strategy, development, rapid deployment, and continuous management.
                     </p>
                 </div>
             ),
